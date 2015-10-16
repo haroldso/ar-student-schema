9fe7b15df972c1a93fc3c7053e2a9b4ebdd46874
Harold So <tongtakent@gmail.com>
2015-10-16 12:09:26 +0800

    created_table & assignment

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,9 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
+
+	def name
+		self.first_name + " " + self.last_name
+	end
 # implement your Student model here
 end
--- db/migrate/20151016110500_create_teachers.rb
+++ db/migrate/20151016110500_create_teachers.rb
@@ -2,14 +2,13 @@ require_relative '../config'
 
 # this is where you should use an ActiveRecord migration to 
 
-class CreateStudents < ActiveRecord::Migration
+class CreateTeachers < ActiveRecord::Migration
   def change
   	create_table : students do |t|
   		t.string :first_name
   		t.string :last_name
-  		t.string :gender
-  		t,date :birthday
   		t.string :email, unique: true
+      t,string :phone
   		t.timestamp null: false
   	end
     # HINT: checkout ActiveRecord::Migration.create_table
